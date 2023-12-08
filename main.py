724ea86fe8132d2627be69d6450211c436dc2890
Shivam Bhawsar <67222848+cho-zen@users.noreply.github.com>
2023-12-08 13:56:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from os import environ as env
+
+app = FastAPI()
+
+
+@app.get('/')
+async def index():
+    return {"details":"Hello World! This is the env -- {env['MYVARIABLE']}"}
+
+
+
+# uvicorn main:app --reload --port=8000
+# uvicorn main:app --reload --port=8000 --host=0.0.0.0
