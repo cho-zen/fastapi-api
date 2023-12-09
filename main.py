2b376375ba6e8da8807e0fbbd962c4b5108e99eb
Shivam Bhawsar <67222848+cho-zen@users.noreply.github.com>
2023-12-09 08:47:24 +0530

    Update main.py -- Adding Firebase Credentials

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI, File, UploadFile
 import uuid
 from fastapi.responses import FileResponse
+import cv2
+import pyrebase
 
 IMGDIR = "static/"
 app = FastAPI()
 
-
 @app.get('/')
 async def index():
     return "Hello Shivam!!"
-
 
 @app.post('/upload')
 async def upload_image(file: UploadFile = File(...)):
@@ -20,9 +20,30 @@ async def upload_image(file: UploadFile = File(...)):
     with open(f"{IMGDIR}{file.filename}","wb") as f:
         f.write(contents)
 
+    img = cv2.imread(f'{IMGDIR}image.jpg')
+    
+    firebase_config = {
+        "apiKey": "AIzaSyCUagURc1l3froPMMFLnUHgIs9eOODB9XI",
+        "authDomain": "survey-application-c9806.firebaseapp.com",
+        "databaseURL": "https://survey-application-c9806-default-rtdb.firebaseio.com",
+        "projectId": "survey-application-c9806",
+        "storageBucket": "survey-application-c9806.appspot.com",
+        "messagingSenderId": "112280167703",
+        "appId": "1:112280167703:web:aadeff65cb8141f52d0352"
+        }
+
+    firebase = pyrebase.initialize_app(firebase_config)
+    db = firebase.database()
+
+    db.child("/ImageData/").set(str(img[:,0,0]))
+
+    # print(img[:,0,0])
+
+    
+
     return FileResponse(f'{IMGDIR}image.jpg')
 
 
 
-# uvicorn main:app --reload --port=8000
+# uvicorn main2:app --reload --port=8000
 # uvicorn main:app --reload --port=8000 --host=0.0.0.0
